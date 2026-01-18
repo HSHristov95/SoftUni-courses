8a09305901607409f369bd9174cb605d7d830683
HSHristov95 <151662253+HSHristov95@users.noreply.github.com>
2026-01-18 14:02:30 +0200

    Add files via upload

---
+++ Orders.py
@@ -0,0 +1,18 @@
+number_of_orders = int(input())
+price = 0
+total_price = 0
+
+for _ in range(1, number_of_orders + 1):
+    price_per_capsule = float(input())
+    days = int(input())
+    capsules_per_day = int(input())
+
+    if 0.01 <= price_per_capsule <= 100:
+        if 1 <= days <= 31:
+            if 1 <= capsules_per_day <= 2000:
+                price = price_per_capsule * days * capsules_per_day
+                total_price += price
+            else:
+                continue
+            print(f'The price for the coffee is: ${price:.2f}')
+print(f'Total: ${total_price:.2f}')
