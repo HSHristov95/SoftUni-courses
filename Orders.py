d5bf7fefb9c20ffba392d9920080068f9f1b167b
HSHristov95 <151662253+HSHristov95@users.noreply.github.com>
2026-01-27 21:43:04 +0200

    Add files via upload

--- Orders.py
+++ Orders.py
@@ -1,18 +1,14 @@
-number_of_orders = int(input())
-price = 0
-total_price = 0
+product = input()
+number_of_products = int(input())
 
-for _ in range(1, number_of_orders + 1):
-    price_per_capsule = float(input())
-    days = int(input())
-    capsules_per_day = int(input())
+def price_tab(order, number):
+    if order == 'coffee':
+        return f'{number * 1.50:.2f}'
+    elif order == 'water':
+        return f'{number * 1.00:.2f}'
+    elif order == 'coke':
+        return f'{number * 1.40:.2f}'
+    elif order == 'snacks':
+        return f'{number * 2.00:.2f}'
 
-    if 0.01 <= price_per_capsule <= 100:
-        if 1 <= days <= 31:
-            if 1 <= capsules_per_day <= 2000:
-                price = price_per_capsule * days * capsules_per_day
-                total_price += price
-            else:
-                continue
-            print(f'The price for the coffee is: ${price:.2f}')
-print(f'Total: ${total_price:.2f}')
+print(price_tab(product, number_of_products))
