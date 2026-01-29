178b4666f57626a9a4d62cb9772a6ceba6e12f0c
HSHristov95 <151662253+HSHristov95@users.noreply.github.com>
2026-01-29 22:41:01 +0200

    Add files via upload

--- Even_numbers.py
+++ Even_numbers.py
@@ -1,10 +1,8 @@
-number = int(input())
+numbers = input().split()
 
-for _ in range(number):
-    num = int(input())
+def even_numbers(n):
+    return n % 2 == 0
 
-    if num % 2 != 0:
-        print(f'{num} is odd!')
-        break
-else:
-    print('All numbers are even.')
+even_nums = list(filter(even_numbers, map(int,numbers)))
+
+print(even_nums)
